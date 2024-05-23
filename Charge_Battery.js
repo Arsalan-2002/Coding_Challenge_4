13e8319981d8f38eacc2c5c22b7bb56b2551634b
Arsalan-2002 <89111268+Arsalan-2002@users.noreply.github.com>
2024-05-23 13:40:33 +0500

    Add files via upload
    Implement chargeBattery method in EV class.

---
+++ Charge_Battery.js
@@ -0,0 +1,48 @@
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} going at ${this.speed} km/h`);
+    }
+}
+
+class EV extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.charge = chargeTo;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.charge -= 1;
+        console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+    }
+}
+
+const tesla = new EV('Tesla', 120, 23);
+
+console.log('Initial state:');
+console.log(`Make: ${tesla.make}`);
+console.log(`Speed: ${tesla.speed} km/h`);
+console.log(`Charge: ${tesla.charge}%`);
+
+tesla.accelerate(); 
+tesla.brake(); 
+
+tesla.chargeBattery(50);
+console.log(`After charging, charge is ${tesla.charge}%`);
+
+tesla.accelerate(); 
